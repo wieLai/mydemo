dbd3b3722e854d4d0f399c541f1812c8a891bbcf
罗林灿 <1778464595@qq.com>
2020-01-08 20:29:15 +0800

    getters的没有传参，和传参的用法

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="app">
-    <h1>我们可以看一下，</h1>
-<span>{{count}}</span>
-<hr>
-<span>{{countadd}}</span>
-<input type="button" value="点击我" @click="handleCilck">
+<span>{{count}}</span><br>
+<!-- <span>{{addcount}}</span> -->
+<input type="button" value="点击我加加" @click="handleClick">
   </div>
 </template>
 
@@ -12,52 +10,34 @@
 export default {
   data () {
     return {
-      count: this.$store.state.count
+      // count: this.$store.state.count
+      // vuex中的计算属性，就是当计算属性那样来用就行，不能用函数调用的形式，this.$store.getters.addcount()
+      // 如果用函数调用的形式，就是要用另外一种写法了的
+      // 没有传参的用法
+      // count: this.$store.getters.addcount
+      // 传参的用法
+      count: this.$store.getters.addcount(99)
     }
   },
   computed: {
-    // 想要视图更新vuex中的的数据，就需要用计算属性监听this.$store.state.count这个依赖项的变化
-    // 为什么一定要计算属性呢？从下面的watch中我们可以看出，data中的数据，需要初始化的，
-    countadd () {
-      return this.$store.state.count
-    }
-  },
-  watch: {
-    // 监听data中的count属性
-    count () {
-      console.log('如果这里输出了,就证明数据已经改变，但是就是没有更新到视图')
-      console.log('没有输出就是data中的数据没有发生变化，不仅仅是视图没有变化，老师说data中的数据需要初始化')
-      console.log(this.count)
-    },
-    // 监听vuex中的数据   监听对象中的属性值
-    '$store.state.count' () {
-      console.log('vuex中的数据发生了变化')
-      console.log(this.$store.state.count)
-    }
-    // 监听一个对象
-    // $store: {
-    //   handler (newV, oldV) {
-    //     console.log(newV.name, oldV.name)
-    //   },
-    //   deep: true
+    // addcount () {
+    //   return this.$store.state.count
     // }
+    addcount () {
+      return this.$store.getters.addcount
+    }
   },
   methods: {
-    handleCilck () {
-      this.$store.commit('updatedCount')
-    },
-    handleAction () {
-      setTimeout(() => {
-        this.$store.dispatch('updatedCount', 10)
-      }, 3000)
+    handleClick () {
+      this.$store.commit('addcount')
     }
   },
   mounted () {
-    this.handleAction()
+    // console.log(this.$store.getters.addcount)
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style>
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,31 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import vuex from 'vuex'
+Vue.use(vuex)
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export default new vuex.Store({
   state: {
     count: 10
   },
+  // getters相当于计算属性,既然说是像计算属性，那我试试改变state中的数据，也就是改变他的依赖项看看
+  // 结果：依赖改变，他就改变
+  // 下面是没有传参的方式,有没有传参，决定了了这个计算属性的写法，和用法
+  getters: {
+    // addcount (state) {
+    //   return state.count + '我是'
+    // }
+    // addcount: state => state.count + '我是'
+    //
+    // 传参
+    addcount (state) {
+      return (num) => {
+        return state.count + num
+      }
+    }
+  },
   mutations: {
-    updatedCount (state, num) {
-      // state.count++
-      state.count += num
+    addcount (state) {
+      state.count++
     }
-  },
-  actions: {
-    updatedCount (context, num) {
-      context.commit('updatedCount', num)
-    }
-  },
-  modules: {
   }
+
 })
