5b8be07a89e40f4cc69f561aa18279bd0b90f92e
罗林灿 <1778464595@qq.com>
2020-01-09 19:59:01 +0800

    action的使用，和组合action的拓展

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,30 @@
 <template>
   <div class="app">
-<ul>
-  <!-- <li v-for="(item,index) in listDome" :key="index">{{item}}</li> -->
-
-  <li v-for="(item,index) in getlist1" :key="index">{{item}}</li><hr>
-  <li v-for="(item,index) in getlist2" :key="index">{{item}}</li><hr>
-  <li v-for="(item,index) in getlist3" :key="index">{{item}}</li><hr>
-  <li v-for="(item,index) in getlist4" :key="index">{{item}}</li>
-</ul>
+<h1>先来一个异步改变数据的普通操作</h1>
+<span>{{count}}</span>
+<input type="button" value="点击我异步改变数据" @click="handleClick">
+<h1>本来想着搞一下组合action的，但是还不知道他有什么用处，老师也还没有怎么讲，</h1>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+
 export default {
   data () {
     return {
-      // 如果多个组件都是需要做这样的筛选操作的话，就是重复的代码了，我们就需要在vuex中直接做操作
-      // listDome: this.$store.state.list.filter((item, index) => {
-      //   return item === 'img'
-      // })
-      // 没有传参的方式
-      // listDome: this.$store.getters.filterDome
-      // 传参的方式,就需要像函数调用那样了
-      // listDome: this.$store.getters.filterDome('p')
-      listDome: this.$store.getters.filterDome('h1')
+      // count: this.$store.state.count
     }
   },
   computed: {
-    //  普通用法就只能一个个地用了,
-    // getlist1 () {
-    //   return this.$store.getters.getlist1
-    // },
-    // getlist2 () {
-    //   return this.$store.getters.getlist2
-    // },
-    // getlist3 () {
-    //   return this.$store.getters.getlist3
-    // },
-    // getlist4 () {
-    //   return this.$store.getters.getlist4
-    // }
-
-    // mapGetters用法,并没有什么的特别，就是提供了一个方法让我们一次就把我们需要的可以一次搞过来
-    ...mapGetters(['getlist1', 'getlist2', 'getlist3', 'getlist4'])
+    count () {
+      return this.$store.state.count
+    }
   },
   methods: {
-
+    // 普通用法
+    // handleClick () {
+    //   this.$store.dispatch('addcount')
+    // }
   },
   mounted () {
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,51 +5,24 @@ Vue.use(vuex)
 export default new vuex.Store({
   // 需求就是需要在组件中（view）只筛选出我需要的
   state: {
-    list: ['p', 'img', 'h1', 'h2', 'div'],
-    list2: [10, 20, 30, 40, 50, 60, 70, 80]
+    count: 10
   },
 
-  getters: {
-    // mapGetters,,,使用 场景就是我们如果有很多的全局计算属性要用到，在组件中一个个地用台麻烦了
-    getlist1 (state) {
-      return state.list2.filter(item => {
-        return item > 50
-      })
-    },
-    getlist2 (state) {
-      return state.list2.filter(item => {
-        return item > 50
-      })
-    },
-    getlist3 (state) {
-      return state.list2.filter(item => {
-        return item > 50
-      })
-    },
-    getlist4 (state) {
-      return state.list2.filter(item => {
-        return item > 50
-      })
-    },
-
-    // getters普通用法
-    // 没有传参的方式
-    // filterDome (state) {
-    //   return state.list.filter((item) => {
-    //     return item === 'img'
-    //   })
+  mutations: {
+    addcount (state) {
+      state.count++
+    }
+  },
+  actions: {
+    // 普通用法
+    // addcount (context) {
+    //   // console.log(context)
+    //   context.commit('addcount')
     // }
-    // filterDome: state => state.list.filter(v => v === 'img')
-
-    // 下面是传参的方式,通过返回一个函数让组件直接调用
-    // filterDome (state) {
-    //   return (params) => {
-    //     return state.list.filter(item => {
-    //       return item === params
-    //     })
-    //   }
+    // context是他本身就传过来的一个对象，我们直接接收并结构，就直接用就可以了
+    // addcount ({ commit }) {
+    //   // console.log(context)
+    //   commit('addcount')
     // }
-    filterDome: state => params => state.list.filter(item => item === params)
   }
-
 })
