0c9c72a230b87a863468f3fa318cc65e80b8ca3e
罗林灿 <1778464595@qq.com>
2020-01-11 10:30:26 +0800

    引用类型深拷贝注意，引用类型内数值交换

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="app">
-<h1>来试试modules的用法，其实他就是有不同的模块的全局数据
-  （如果所以的全局数据都放在一个模块中，那那个模块也太臃肿了），
-  把不同的模块组合起来</h1>
-<!-- 先试试使用数据 -->
-<span>user模块的{{userCount}}</span> <hr>
-<span>cart模块的{{cartCount}}</span> <hr>
-<input type="button" value="点击我改变user模块中的数据" @click="handleuser">
-<input type="button" value="点击我改变cart模块中的数据" @click="handlecart">
+<h1>这个是深拷贝的一些技巧，数值交换</h1>
+<span>平时项目的时候，不注意引用类型和原始类型的赋值传递的区别很容易造成错误，注意！！！！！</span><br><hr>
+<input type="button" value="点击我" @click="handleClick">
   </div>
 </template>
 
@@ -19,30 +14,43 @@ export default {
 
     }
   },
-  computed: {
-    // 使用的时候就是多加一层.
-    userCount () {
-      return this.$store.state.user.userCount
-    },
-    cartCount () {
-      return this.$store.state.cart.cartCount
-    }
-  },
+
   methods: {
-    // 改变user模块中的数据
-    handleuser () {
-      // 先要改变其中对应模块中的数据的时候，就可以像下面那样多加一层路径
-      this.$store.commit('user/adduserCount', 30)
-    },
-    // 改变cart模块中的数据
-    handlecart () {
-      this.$store.commit('cart/addcartCount', 50)
+    handleClick () {
+      // let num1 = 'aaa'
+      // let num2 = num1
+      // console.log(num1 === num2) // true  原始类型交换的是真正的值，
+
+      // let obj1 = { name: '罗' }
+      // let obj2 = obj1
+      // console.log(obj1 === obj2) // true  引用类型交换的是指针，
+
+      // 那我们对一些引用类内的值进行交换的时候就需要注意了
+      // 下面是最简洁的深拷贝之一
+      // let obj = { name: 'Luo' }
+      // obj.handle = function () {
+      //   console.log('momoda')
+      // }
+      // let obj2 = JSON.parse(JSON.stringify(obj))
+      // console.log(obj === obj2)
+      // // false   简洁是简洁了，但是如果被拷贝的对象中的成员是函数的话， 被拷贝过来的会被丢失
+      // console.log(obj)
+      // console.log(obj2)
+
+      // es6中的数值交换,,需求，我们要下面这个对象中的两个值进行交换
+      let obj1 = { num1: 10, num2: 20 }
+      // 如果我们没有进行深拷贝，，那我们就很可能这样写
+      // obj1.num1 = obj1.num2 // 在这一步的时候，num2已经等于num1了，下面的再交换已经没意义了
+      // obj1.num2 = obj1.num1
+      // console.log(obj1) // {num1: 20, num2: 20}
+
+      // es6
+      console.log(obj1);// {num1: 10, num2: 20}
+      [obj1.num1, obj1.num2] = [obj1.num2, obj1.num1] // 使用这个东西的前面语句记得加上;要不报错都不知道怎么回事
+      console.log(obj1) // {num1: 20, num2: 10}
     }
-  },
-  mounted () {
-    // 如果不知道他的层次，可以直接把this.$store打印出来看看
-    console.log(this.$store)
   }
+
 }
 </script>
 
