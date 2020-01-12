c1e817200cfd6c92536aa1993a123fe96efb5651
罗林灿 <1778464595@qq.com>
2020-01-12 09:44:57 +0800

    es6---demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
-<h1>我们来做一个关于时间的加减法</h1>
+<h1>es6</h1>
+
   </div>
 </template>
 
@@ -9,57 +10,19 @@
 export default {
   data () {
     return {
-      startTime: '2019-10-30 22:50:00',
-      endTime: '2019-10-31 00:00:00'
+      obj: {
+        name: '罗',
+        sex: '难',
+        old: 22,
+        school: '华夏',
+        girlfirend: '婷'
+      }
     }
   },
-  // mounted () {
-  //   let startTime = new Date(this.startTime)
-  //   let endTime = new Date(this.endTime)
-  //   console.log(startTime) // Wed Oct 30 2019 22:50:00 GMT+0800 (中国标准时间)
-  //   console.log(endTime) // Thu Oct 31 2019 00:00:00 GMT+0800 (中国标准时间
-
-  //   let duration = endTime - startTime
-  //   console.log(duration) // 得到是一个毫秒数4200000
-
-  //   // 拿到毫秒数就可以进行换算了，1s===1000ms
-  //   let hour = parseInt(duration / 1000 / 60 / 60)
-  //   console.log(hour) // 1.1666666666666667 // 1
-
-  //   // let min = (duration - hour * 1000 * 60 * 60) / 1000 / 60
-  //   // console.log(min) // 10
-
-  //   let minutes = duration / 1000 / 60 - hour * 60
-  //   console.log(minutes) // 10
-
-  //   console.log(`${hour}时${minutes}分`)
-  // },
-  created () {
-    let startTime = '15:20'
-    let endTime = '08:08'
-    // let startTime = '18:20'
-    // let endTime = '20:30'
-
-    let startTime0 = +startTime.split(':')[0]
-    let startTime1 = +startTime.split(':')[1]
-    console.log(startTime0)// 15
-    console.log(startTime1)// 20
-
-    let endTime0 = +endTime.split(':')[0]
-    let endTime1 = +endTime.split(':')[1]
-    console.log(endTime0) // 08
-    console.log(endTime1)// 08
-
-    let min = endTime1 - startTime1
-    let hour = endTime0 - startTime0
-    if (hour < 0) {
-      hour += 24
-    }
-    if (min < 0) {
-      hour--
-      min += 60
-    }
-    console.log(`${hour}时${min}`)
+  mounted () {
+    let { girlfirend, ...obj2 } = this.obj
+    console.log(girlfirend)
+    console.log(obj2) // 可以解构出变量，也可以找出剩余的对象
   }
 
 }
