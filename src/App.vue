db066235214f4f46610a8020c457a191a2b83ee0
罗林灿 <1778464595@qq.com>
2020-01-12 10:32:36 +0800

    数组相关操作

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-<h1>es6</h1>
+<h1>数组api</h1>
 
   </div>
 </template>
@@ -10,19 +10,15 @@
 export default {
   data () {
     return {
-      obj: {
-        name: '罗',
-        sex: '难',
-        old: 22,
-        school: '华夏',
-        girlfirend: '婷'
-      }
+
     }
   },
   mounted () {
-    let { girlfirend, ...obj2 } = this.obj
-    console.log(girlfirend)
-    console.log(obj2) // 可以解构出变量，也可以找出剩余的对象
+    // slice() 方法返回一个新的数组对象，这一对象是一个由
+    // begin 和 end 决定的原数组的浅拷贝（包括 begin，不包括end）。原始数组不会被改变。
+
+    // splice() 方法通过删除或替换现有元素或者原地添加新的元素来修改数组,
+    // 并以数组形式返回被修改的内容。此方法会改变原数组。
   }
 
 }
