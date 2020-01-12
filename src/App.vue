70328e7d6eb51725d5c927159c0b0368ca61f96f
罗林灿 <1778464595@qq.com>
2020-01-12 09:39:01 +0800

    我们来做一个关于时间的加减法

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="app">
-<h1>这个是深拷贝的一些技巧，数值交换</h1>
-<span>平时项目的时候，不注意引用类型和原始类型的赋值传递的区别很容易造成错误，注意！！！！！</span><br><hr>
-<input type="button" value="点击我" @click="handleClick">
+<h1>我们来做一个关于时间的加减法</h1>
   </div>
 </template>
 
@@ -11,49 +9,62 @@
 export default {
   data () {
     return {
-
+      startTime: '2019-10-30 22:50:00',
+      endTime: '2019-10-31 00:00:00'
     }
   },
+  // mounted () {
+  //   let startTime = new Date(this.startTime)
+  //   let endTime = new Date(this.endTime)
+  //   console.log(startTime) // Wed Oct 30 2019 22:50:00 GMT+0800 (中国标准时间)
+  //   console.log(endTime) // Thu Oct 31 2019 00:00:00 GMT+0800 (中国标准时间
 
-  methods: {
-    handleClick () {
-      // let num1 = 'aaa'
-      // let num2 = num1
-      // console.log(num1 === num2) // true  原始类型交换的是真正的值，
+  //   let duration = endTime - startTime
+  //   console.log(duration) // 得到是一个毫秒数4200000
 
-      // let obj1 = { name: '罗' }
-      // let obj2 = obj1
-      // console.log(obj1 === obj2) // true  引用类型交换的是指针，
+  //   // 拿到毫秒数就可以进行换算了，1s===1000ms
+  //   let hour = parseInt(duration / 1000 / 60 / 60)
+  //   console.log(hour) // 1.1666666666666667 // 1
 
-      // 那我们对一些引用类内的值进行交换的时候就需要注意了
-      // 下面是最简洁的深拷贝之一
-      // let obj = { name: 'Luo' }
-      // obj.handle = function () {
-      //   console.log('momoda')
-      // }
-      // let obj2 = JSON.parse(JSON.stringify(obj))
-      // console.log(obj === obj2)
-      // // false   简洁是简洁了，但是如果被拷贝的对象中的成员是函数的话， 被拷贝过来的会被丢失
-      // console.log(obj)
-      // console.log(obj2)
+  //   // let min = (duration - hour * 1000 * 60 * 60) / 1000 / 60
+  //   // console.log(min) // 10
 
-      // es6中的数值交换,,需求，我们要下面这个对象中的两个值进行交换
-      let obj1 = { num1: 10, num2: 20 }
-      // 如果我们没有进行深拷贝，，那我们就很可能这样写
-      // obj1.num1 = obj1.num2 // 在这一步的时候，num2已经等于num1了，下面的再交换已经没意义了
-      // obj1.num2 = obj1.num1
-      // console.log(obj1) // {num1: 20, num2: 20}
+  //   let minutes = duration / 1000 / 60 - hour * 60
+  //   console.log(minutes) // 10
 
-      // es6
-      console.log(obj1);// {num1: 10, num2: 20}
-      [obj1.num1, obj1.num2] = [obj1.num2, obj1.num1] // 使用这个东西的前面语句记得加上;要不报错都不知道怎么回事
-      console.log(obj1) // {num1: 20, num2: 10}
+  //   console.log(`${hour}时${minutes}分`)
+  // },
+  created () {
+    let startTime = '15:20'
+    let endTime = '08:08'
+    // let startTime = '18:20'
+    // let endTime = '20:30'
+
+    let startTime0 = +startTime.split(':')[0]
+    let startTime1 = +startTime.split(':')[1]
+    console.log(startTime0)// 15
+    console.log(startTime1)// 20
+
+    let endTime0 = +endTime.split(':')[0]
+    let endTime1 = +endTime.split(':')[1]
+    console.log(endTime0) // 08
+    console.log(endTime1)// 08
+
+    let min = endTime1 - startTime1
+    let hour = endTime0 - startTime0
+    if (hour < 0) {
+      hour += 24
     }
+    if (min < 0) {
+      hour--
+      min += 60
+    }
+    console.log(`${hour}时${min}`)
   }
 
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 
 </style>
